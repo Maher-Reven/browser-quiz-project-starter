d06dd5327f0679fd22ad47f3fad8fd5a40ff5bb6
HebaSalem <puupiiiii@gmail.com>
2022-10-10 23:29:54 +0200

    finish score task

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -11,14 +11,19 @@ export const createAnswerElement = (key, answerText , currentQuestion) => {
   element.innerHTML = String.raw`
     ${key}: ${answerText};
   `;
+
   element.classList.add('answer-item')
   element.id = key
+
   element.addEventListener('click' , myFun)
+
   function myFun() {
     const parent = element.parentNode
     Array.from(parent.children).forEach(child => (child.classList = 'answer-item'))
+    
     element.classList.add('selected')
     currentQuestion.selected = key
+    
     currentQuestion.correct === currentQuestion.selected && (score += 1)
     document.getElementById('userScore').innerText = `Score: ${score}`
 
@@ -27,7 +32,9 @@ export const createAnswerElement = (key, answerText , currentQuestion) => {
     } else {
       element.classList.add('wrong')
     }
+    Array.from(parent.children).forEach(child => (child.style.pointerEvents = "none"))
   }
+
   return element;
 };
 
