b0f0c65357f1688eae91e5e98abc21ad918f873e
HebaSalem <puupiiiii@gmail.com>
2022-10-11 15:22:07 +0200

    update red-wrong-answer branch code

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -9,16 +9,21 @@ export const createAnswerElement = (key, answerText , currentQuestion) => {
   element.innerHTML = String.raw`
     ${key}: ${answerText};
   `;
+
   element.classList.add('answer-item')
+
   element.addEventListener('click' , ()=> {
     const parent = element.parentNode
-    Array.from(parent.children).forEach(child => child.classList.remove('selected'))
+    Array.from(parent.children).forEach(child => (child.classList = 'answer-item'))
+
     element.classList.add('selected')
+    
     if(key === currentQuestion.correct) {
       element.classList.add('right')
     } else {
       element.classList.add('wrong')
     }
   })
+
   return element;
 };
