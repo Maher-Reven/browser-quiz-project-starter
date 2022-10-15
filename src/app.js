a884ab48cfa0cfe064578ed7323892eb68ada97d
necati <necati.arslan2018@gmail.com>
2022-10-15 07:50:37 +0200

    local-stroge-preserve-current-question-when-refresh-page

--- src/app.js
+++ src/app.js
@@ -2,11 +2,19 @@
 
 import { quizData } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
+import { initQuestionPage } from './pages/questionPage.js';
 
 const loadApp = () => {
   quizData.currentQuestionIndex = 0;
 
-  initWelcomePage();
+  const currentQuestion = localStorage.getItem('currentIndex');
+
+  if (currentQuestion) {
+    quizData.currentQuestionIndex = currentQuestion;
+    initQuestionPage();
+  } else {
+    initWelcomePage();
+  }
 };
 
 window.addEventListener('load', loadApp);
